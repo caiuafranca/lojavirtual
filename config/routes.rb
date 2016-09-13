d5e182ea7d933a8ca19d87bc326728ff782e080a
Caiua Franca <caiua@unimednatal.com.br>
2016-09-13 08:59:29 -0300

    Add Pages Statics About, Contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :microposts
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'users#index'
+  root to: 'pages#home'
+  get 'pages/about'
+  get 'pages/contact'
 end
