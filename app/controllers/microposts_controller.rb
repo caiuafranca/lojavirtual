6d0b3bafb79a93f548bb53c4681b7ff5914fe502
Caiua Franca <caiua@unimednatal.com.br>
2016-09-22 16:43:03 -0300

    criação do twitter

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -4,7 +4,7 @@ class MicropostsController < ApplicationController
   # GET /microposts
   # GET /microposts.json
   def index
-    @microposts = Micropost.all
+    @microposts = Micropost.joins(:user).all
   end
 
   # GET /microposts/1
